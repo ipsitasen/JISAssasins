87da6f05c226093eb62d31e7edcbabfec7a7f5c6
ipsitasen <46235877+ipsitasen@users.noreply.github.com>
2018-12-29 16:07:38 +0530

    Add files via upload

---
+++ ques13.py
@@ -0,0 +1,8 @@
+#question no 19by ipsita sengupta
+x=int(input('enter  the 1st sides value of triangle'))
+y=int(input('enter the 2nd sides value of triangle'))
+z=int(input('enter the 3rd slides value of triangle'))
+if(z>=x+y):
+    print("this is a valid triangle")
+else:
+    print("this is a invallid triangle")
